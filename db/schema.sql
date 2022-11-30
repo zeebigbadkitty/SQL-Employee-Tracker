c5663899acf3823c5ec257c0ab8111f2b4f355a1
Socially Awkward Streams <candice.radam@gmail.com>
2022-11-29 20:12:00 -0600

    Created the Schema.

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS catalog_db;
+CREATE DATABASE catalog_db;
+
+USE catalog_db;
+
+CREATE TABLE departments (
+  id INT NOT NULL,
+  date_updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE roles (
+  role_id INT NOT NULL,
+  department VARCHAR(30) NOT NULL,
+  salary INT NOT NULL,
+  date_updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+CREATE TABLE employees (
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  job_title VARCHAR(30) NOT NULL,
+  department VARCHAR(30) NOT NULL,
+  salaries INT NOT NULL,
+  manager VARCHAR(30) NOT NULL,
+  date_updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
---
+++ index.js
@@ -0,0 +1,5 @@
+//View all: Departments, roles, employees, add a department, add a role, add an employee and update their role.
+//When adding a department: Enter the name of the department.
+//When you add a role: Name, Salary, Department.
+//When adding an employee: First, Last, Role, Manager.
+//When updating the role: prompted to select employee and update their new role. 
